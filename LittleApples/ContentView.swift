8c049e629be9463fe74e45faa584c75e6a2e6237
Anika Amin <anika43210@gmail.com>
2023-08-16 09:47:12 -0400

    I wrote Hello

--- LittleApples/ContentView.swift
+++ LittleApples/ContentView.swift
@@ -16,25 +16,7 @@ struct ContentView: View {
                 .cornerRadius(15)
                 .padding(45)
             VStack {
-//Title
-                Text("Welcome to Little Apple")
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
-                    .multilineTextAlignment(.center)
-                    .padding(30)
-                Image("littleApple")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .padding(140)
-//Catch phrase
-                Text("Don’t know what to do in NYC ?")
-                    .font(.title2)
-                    .multilineTextAlignment(.center)
-                    .padding(50)
-                Text("Next")
-                    .font(.largeTitle)
-                    .multilineTextAlignment(.center)
-
+Text("Hello")
             }
         }
     }
