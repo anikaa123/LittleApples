50694862f235c8f315e8b20d2365c4ac0e817e17
Anika Amin <anika43210@gmail.com>
2023-08-16 12:35:54 -0400

    Little Apples FIRST information page template.

--- LittleApples/ContentView.swift
+++ LittleApples/ContentView.swift
@@ -8,16 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    let deviceBg = #colorLiteral(red: 0.9655330777, green: 0.8915256858, blue: 0.7311252952, alpha: 1)
     var body: some View {
         ZStack{
-            Color.red
+            Color(deviceBg)
                 .ignoresSafeArea()
-            Color.white
-                .cornerRadius(15)
-                .padding(45)
-            VStack {
-Text("Hello")
-            }
+            
+//Title:
+        VStack {
+            Text("Results for shopping in Brooklynn")
+                .font(.title)
+                .fontWeight(.medium)
+                .multilineTextAlignment(.center)
+                .padding(30)
+//First locatoin
+            Text("Name of place")
+                .font(.title3)
+        HStack{
+            Image("blankImage")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 130.0)
+            
+            VStack(alignment: .leading){
+                Text("Location:")
+                Text("Hours:")
+                Text("Rating:")
+            }.padding(.leading)
+                }
+//padding1
+        .padding(.bottom, 20)
+        .padding(.trailing, 100)
+            
+//Second locatoin
+        Text("Name of place")
+            .font(.title3)
+        HStack{
+            Image("blankImage")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 130.0)
+            VStack(alignment: .leading){
+                Text("Location:")
+                Text("Hours:")
+                Text("Rating:")
+            }.padding(.leading)
+                }
+//padding2
+        .padding(.bottom, 20)
+        .padding(.trailing, 100)
+            
+//Third locatoin
+            Text("Name of place")
+                .font(.title3)
+        HStack{
+            Image("blankImage")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 130.0)
+            VStack(alignment: .leading){
+                Text("Location:")
+                Text("Hours:")
+                Text("Rating:")
+            }.padding(.leading)
+                }
+//padding3
+        .padding(.trailing, 100)
+        }
+        .foregroundColor(Color(red: 0.77, green: 0.344, blue: 0.144))
         }
     }
     
